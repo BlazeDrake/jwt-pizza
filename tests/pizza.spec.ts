127ead488e7efc03c609f73281d49f66d4c2ce61
BlazeDrake <blaze.drake@outlook.com>
2025-10-02 10:36:49 -0600

    Added navigation & registration tests
    Added tests for registering a new user, as well as basic navigation

--- tests/pizza.spec.ts
+++ tests/pizza.spec.ts
@@ -119,3 +119,72 @@ test('purchase with login', async ({ page }) => {
   await expect(page.getByText('0.008')).toBeVisible();
 });
 
+test('basic navigation', async ({page})=>{
+  await page.goto('/');
+  await page.getByRole('link', { name: 'History' }).click();
+  await expect(page.getByRole('heading')).toContainText('Mama Rucci, my my');
+  await expect(page.getByRole('list')).toContainText('history');
+  await page.getByRole('link', { name: 'About' }).click();
+  await expect(page.getByRole('list')).toContainText('about');
+  await expect(page.getByRole('main')).toContainText('The secret sauce');
+  await page.getByRole('contentinfo').getByRole('link', { name: 'Franchise' }).click();
+  await expect(page.getByRole('list')).toContainText('franchise-dashboard');
+  await expect(page.getByRole('alert')).toContainText('If you are already a franchisee, pleaseloginusing your franchise account');
+})
+
+test('unknown-page', async ({page})=>{
+  await page.goto('/badpage');
+  await expect(page.getByRole('heading')).toContainText('Oops');
+})
+
+test('register-then-logout', async ({page})=>{
+  await page.route('http://localhost:3000/api/auth', async (route)=>{
+    if(route.request().method()==="POST"){
+        const registerRes={
+        "user": {
+          "name": "testUser",
+          "email": "test@a.com",
+          "roles": [
+            {
+              "role": "diner"
+            }
+          ],
+          "id": 519
+        },
+        "token": "aTestToken"
+      }
+      await route.fulfill({ json: registerRes });
+    }
+    else if(route.request().method()==="DELETE"){
+      const logoutRes={
+        "message": "logout successful"
+      }
+      await route.fulfill({ json: logoutRes });
+    }
+
+  })
+
+
+  await page.goto('/');
+
+  //register
+  await page.getByRole('link', { name: 'Register' }).click();
+  await page.getByRole('textbox', { name: 'Full name' }).fill('testUser');
+  await page.getByRole('textbox', { name: 'Email address' }).click();
+  await page.getByRole('textbox', { name: 'Email address' }).fill('test@a.com');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('totallySeecure');
+  await page.getByRole('button', { name: 'Register' }).click();
+
+  //ensure login is successful
+  await expect(page.locator('#navbar-dark')).toContainText('Logout');
+  await page.getByRole('link', { name: 't', exact: true }).click();
+  await expect(page.getByRole('main')).toContainText('testUser');
+  await expect(page.getByRole('main')).toContainText('test@a.com');
+  await expect(page.getByRole('main')).toContainText('diner');
+
+  //ensure logout is successful
+  await page.getByRole('link', { name: 'Logout' }).click();
+  await expect(page.locator('#navbar-dark')).toContainText('Login');
+})
+
